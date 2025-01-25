a0d19883bfa8bb30aeeb91e982ee0812d49ed227
ShreyasP10 <shreyaspawar1011@gmail.com>
2025-01-25 23:37:06 +0530

    Initialization

---
+++ app/src/main/java/com/shreyaspawar/geoguide/MainActivity.java
@@ -0,0 +1,251 @@
+package com.shreyaspawar.geoguide;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.hardware.GeomagneticField;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageButton;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.Priority;
+import com.shreyaspawar.geoguide.databinding.ActivityMainBinding;
+
+import java.io.IOException;
+import java.text.MessageFormat;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private ActivityMainBinding binding;
+    private SensorManager sensorManager;
+    private Sensor accelerometer, magnetometer;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private Location currentLocation;
+    private float[] gravity;
+    private float[] geomagnetic;
+    private float currentDegree = 0f;
+
+    private final ActivityResultLauncher<String> permissionLauncher = registerForActivityResult(
+            new ActivityResultContracts.RequestPermission(),
+            isGranted -> {
+                if (isGranted) {
+                    getLocation();
+                } else {
+                    Log.e("Permission", "Location permission denied.");
+                }
+            }
+    );
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        initSensors();
+        initLocationProvider();
+
+        // Check for location permission
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION);
+        } else {
+            getLocation();
+        }
+
+        ImageButton infoButton = findViewById(R.id.infoButton);
+
+        // Set an OnClickListener
+        infoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to the NextActivity
+                Intent intent = new Intent(MainActivity.this, infoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void initSensors() {
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+    }
+
+    private void initLocationProvider() {
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getLocation() {
+        LocationRequest locationRequest = new LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY)
+                .setIntervalMillis(5000)
+                .setMinUpdateIntervalMillis(1000)
+                .setMaxUpdates(1)
+                .build();
+
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, new LocationCallback() {
+            @Override
+            public void onLocationResult(@NonNull LocationResult locationResult) {
+                currentLocation = locationResult.getLastLocation();
+                if (currentLocation != null) {
+                    updateLocationUI();
+                }
+            }
+        }, null);
+    }
+
+    private void updateLocationUI() {
+        if (currentLocation == null) return;
+
+        binding.latitudeTV.setText(MessageFormat.format("Latitude: {0}", currentLocation.getLatitude()));
+        binding.longitudeTV.setText(MessageFormat.format("Longitude: {0}", currentLocation.getLongitude()));
+        double latitude_value=currentLocation.getLatitude();
+        double gravity_value=calculateGravity(latitude_value);
+        binding.trueHeadingTV.setText(MessageFormat.format("Gravity: {0}", gravity_value));
+        getCityName(currentLocation);
+    }
+
+    private void getCityName(Location location) {
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        try {
+            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            if (addresses != null && !addresses.isEmpty()) {
+                binding.cityTV.setText(addresses.get(0).getLocality());
+            } else {
+                binding.cityTV.setText("City not found");
+            }
+        } catch (IOException e) {
+            Log.e("Geocoder", "Error getting city name", e);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+        sensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_FASTEST);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            gravity = event.values;
+        } else if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            geomagnetic = event.values;
+        }
+
+        if (gravity != null && geomagnetic != null) {
+            float[] R = new float[9];
+            float[] I = new float[9];
+            if (SensorManager.getRotationMatrix(R, I, gravity, geomagnetic)) {
+                float[] orientation = new float[3];
+                SensorManager.getOrientation(R, orientation);
+
+
+                float declination = 0;
+                if (currentLocation != null) {
+                    GeomagneticField geomagneticField = new GeomagneticField(
+                            (float) currentLocation.getLatitude(),
+                            (float) currentLocation.getLongitude(),
+                            (float) currentLocation.getAltitude(),
+                            System.currentTimeMillis()
+                    );
+                    declination = geomagneticField.getDeclination();
+                }
+                float azimuthInDegrees = (float) Math.toDegrees(orientation[0]);
+                azimuthInDegrees = (azimuthInDegrees + 360) % 360; // Normalize to [0, 360]
+                float trueNorth = (azimuthInDegrees + declination) % 360;
+
+
+                float trueHeading = (azimuthInDegrees + declination + 360) % 360;
+
+                // Update UI
+//                binding.trueHeadingTV.setText(MessageFormat.format("{0}°", Math.round(trueHeading)));
+                binding.headingTV.setText(MessageFormat.format("{0}°", Math.round(azimuthInDegrees)));
+                binding.directionTV.setText(getDirection(azimuthInDegrees));
+
+                // Compass animation
+                RotateAnimation rotateAnimation = new RotateAnimation(
+                        currentDegree,
+                        trueHeading,
+                        RotateAnimation.RELATIVE_TO_SELF, 0.5f,
+                        RotateAnimation.RELATIVE_TO_SELF, 0.5f
+                );
+                currentDegree = trueHeading;
+
+                rotateAnimation.setDuration(210);
+                rotateAnimation.setFillAfter(true);
+
+                binding.compassView.startAnimation(rotateAnimation);
+                currentDegree = -azimuthInDegrees;
+            }
+        }
+
+    }
+
+
+
+    private String getDirection(float degree) {
+        if (degree > 22.5 && degree <= 67.5) return "NE";
+        if (degree > 67.5 && degree <= 112.5) return "E";
+        if (degree > 112.5 && degree <= 157.5) return "SE";
+        if (degree > 157.5 && degree <= 202.5) return "S";
+        if (degree > 202.5 && degree <= 247.5) return "SW";
+        if (degree > 247.5 && degree <= 292.5) return "W";
+        if (degree > 292.5 && degree <= 337.5) return "NW";
+        return "N";
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Optional: Handle changes in sensor accuracy.
+    }
+
+    public static double calculateGravity(double latitude_value) {
+        double g0 = 9.780327; // Gravity at the equator (m/s^2)
+        double k = 0.00193185138639; // Flattening coefficient
+        double e2 = 0.00669437999013; // Eccentricity squared
+        double sinLat = Math.sin(Math.toRadians(latitude_value));
+
+        return g0 * (1 + k * sinLat * sinLat) / Math.sqrt(1 - e2 * sinLat * sinLat);
+    }
+}
---
+++ app/src/main/java/com/shreyaspawar/geoguide/infoActivity.java
@@ -0,0 +1,31 @@
+package com.shreyaspawar.geoguide;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class infoActivity extends AppCompatActivity {
+    Button backToMain;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_info);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        backToMain=findViewById(R.id.backToMain);
+        backToMain.setOnClickListener(v -> {
+            finish();
+        });
+
+    }
+
+}
