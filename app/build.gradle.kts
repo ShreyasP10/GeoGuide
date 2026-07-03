48adf53fcfe7e20508e40ef0414b9dbc9d077c6f
ShreyasP10 <shreyaspawar1011@gmail.com>
2026-07-04 00:28:32 +0530

    Fix string typos and bump version to 4.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.shreyaspawar.geoguide"
         minSdk = 30
         targetSdk = 34
-        versionCode = 4
-        versionName = "4.0"
+        versionCode = 5
+        versionName = "4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
